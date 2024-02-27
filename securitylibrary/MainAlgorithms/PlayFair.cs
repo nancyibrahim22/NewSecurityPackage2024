8e109ffbf040e48b2ac198f08cb6ecf23bf7f1c9
x x <20201700449@cis.asu.edu.eg>
2024-02-27 15:56:28 +0200

    mjoisgrfd

--- securitylibrary/MainAlgorithms/PlayFair.cs
+++ securitylibrary/MainAlgorithms/PlayFair.cs
@@ -15,6 +15,7 @@ namespace SecurityLibrary
             //  Console.WriteLine(cipherText);
             /*for(int i = 0;i<5;i++)
             {
+
                 for(int j=0;j<5;j++)
                     Console.Write(newmatrix[i,j]);
                 Console.WriteLine();
