f474c15560bdf8337adecfb06f49c2f3e2edafe8
Nouran936 <112628931+Nouran936@users.noreply.github.com>
2024-05-08 17:51:38 +0300

    All algorithms are done

--- securitylibrary/DiffieHellman/DiffieHellman.cs
+++ securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -12,15 +12,43 @@ namespace SecurityLibrary.DiffieHellman
     public class DiffieHellman
     {
 
-      public  int power(int f, int s, int sf)
+        public int power(int f, int s, int sf)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            int key = 1;
+            int i = 0;
+            while (i < s)
+            {
+                key *= f;
+                key %= sf;
+                i++;
+            }
+            return key;
         }
 
-       public List<int> GetKeys(int q, int alpha, int xa, int xb)
+        public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
 
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            List<int> outputs = new List<int>();
+
+            int publicA = 0;
+            publicA = power(alpha, xa, q);
+
+            int publicB = 0;
+            publicB = power(alpha, xb, q);
+
+
+            int secreteA = 0;
+            secreteA = power(publicB, xa, q);
+
+            int secreteB = 0;
+            secreteB = power(publicA, xb, q);
+
+            outputs.Add(secreteA);
+            outputs.Add(secreteB);
+
+            return outputs;
         }
     }
 }
